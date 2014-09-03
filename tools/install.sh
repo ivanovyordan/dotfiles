163c3b84661b3dd5d997004dc4b28a42e7c237e8
Yordan Ivanov <mail@ivanovyordan.com>
2014-09-03 14:04:16 +0300

    modified the installer

--- tools/install.sh
+++ tools/install.sh
@@ -1,96 +1,143 @@
 #!/bin/bash
 echo "Dotfiles - Yordan Ivanov"
 
-# APT repositories
-sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo add-apt-repository ppa:nilarimogard/webupd8
+# APT Repositories
+apt_repositories=(
+	"ppa:git-core/ppa" # git
+	"ppa:webupd8team/sublime-text-3" # Sublime text 3
+	"ppa:versable/elementary-update" # Customization
+	"ppa:numix/ppa" # Numix icons
+	"ppa:moka/stable" # Moka icons
+	"deb http://dl.google.com/linux/chrome/deb/ stable main" # Google Chrome
+)
 
-# Add Google Chrome to sources list
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+# Packages
+packages=(
+	# Compilers
+	build-essential
+	libgl1-mesa-dev # Qt related
+	libglu1-mesa-dev # Qt related
+	mesa-common-dev # Qt related
 
-# Update the system
-sudo apt-get -qq -y update && sudo apt-get -y upgrade
-
-# Install packages
-sudo apt-get install\
-	autojump\
-	build-essential\
-	curl\
-	colordiff\
-	dropbox\
-	freshplayerplugin\
-	git-core\
-	git-flow\
-	git-extras\
-	google-chrome-stable\
-	lamp-server^\
-	libgl1-mesa-dev\
-	libglu1-mesa-dev\
-	meld\
-	mesa-common-dev\
-	openssh-server\
-	phpmyadmin\
-	python-gpgme\
-	skype\
-	steam-launcher\
-	subversion\
-	sublime-text-installer\
-	vim\
-	vlc\
+	# CLI
+	autojump
+	curl
 	zsh
 
-# Install vundle
+	# VCS
+	git-core
+	git-flow
+
+	python-gpgme
+	dropbox
+
+	# Text Editors
+	sublime-text-installer
+	vim
+
+	# Text diff
+	meld
+	colordiff
+
+	# WEB
+	curl
+	google-chrome-stable
+	firefox
+	thunderbird
+	adobe-flashplugin
+	icedtea-plugin
+	deluge
+
+	# Fun
+	skype
+	vlc
+	foto
+	steam-launcher
+	ubuntu-restricted-extras
+
+	# Customization
+	elementary-tweaks
+	elementary-artwork
+	elementary-wallpaper-collection
+	numix-icon-theme-circle
+	faba-colors
+	faba-icon-theme
+	faba-mono-icons
+	elementary-*-theme
+	elementary-*-icons
+)
+
+npm_packages=(
+	grunt-cli
+	bower
+	less
+	jshint
+	csslint
+	uglify-js
+	hexo
+)
+
+gems=(
+	sass
+	compass
+)
+
+echo 'Add new software repositories'
+for repository in "${apt_repositories[@]}"
+do
+	sudo add-apt-repository -y $repository
+done
+
+echo 'Update repositories'
+sudo apt-get -qq -y update && sudo apt-get -y upgrade
+
+echo 'Install packages'
+packages=$(IFS=$' '; echo "${packages[*]}")
+apt-get install -y $packages
+
+echo 'Install git-extras'
+(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install)
+
+echo 'Install vundle'
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-# Install RVM
+echo 'Install NVM'
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
+source $HOME/.nvm/nvm.sh
+nvm install 0.10
+nvm use 0.10
+nvm alias default 0.10
+
+echo 'Install Node.js packages'
+npm_packages=$(IFS=$' '; echo "${npm_packages[*]}")
+apt-get install -y $npm_packages
+
+echo 'Install RVM'
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+source $HOME/.rvm/scripts/rvm
 
-# Install NVM
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
+echo 'Install Ruby gems'
+gems=$(IFS=$' '; echo "${gems[*]}")
+apt-get install -y $gems
 
-# Install Node.js
-nvm install 0.10.28
-nvm use 0.10.28
-nvm alias default 0.10.28
-
-# Install Ruby gems
-gem install\
-	sass\
-	compass
-
-# Install Node.js packages
-npm install -g\
-	grunt-cli\
-	bower\
-	less\
-	jshint\
-	csslint\
-	uglify-js\
-	hexo
-
-# Clone the .dotfiles
+echo 'Clone the .dotfiles'
 git clone --recurse-submodules https://github.com/ivanov-yordan/dotfiles.git $HOME/.dotfiles
+cd $HOME/.dotfiles
+git submodule foreach git pull origin master
 
-# Link default theme
-ln -s $HOME/.dotfiles/custom/ivanov-yordan.zsh-theme $HOME/.dotfiles/oh-my-zsh/themes/
-
-# Link files
+echo 'Link files'
 rm $HOME/.zshrc
-find $HOME/.dotfiles/link/ -maxdepth 1 -print "%P\n" | while read file; do ln -s "$HOME/.dotfiles/link/$file" "$HOME/$file"; done
 
-# Configure Apache
-sudo ln -s /var/www/html $HOME/public_html
-sudo adduser $USER www-data
-sudo chown -R $USER:www-data $HOME/public_html
-sudo a2enmod rewrite
-sudo cp /etc/apache2/sites-available/default{,.backup}
-udo sed -i "s/AllowOverride\ None/None \None/g" /etc/apache2/sites-available/default
-sudo service apache2 restart
+folder="$HOME/.dotfiles/link"
+files=`ls -a "$folder"`
+for file in $files; do
+	if [[ "$file" != "." && "$file" != ".." ]]; then
+		ln -s "$folder/$file" "$HOME/$file"
+	fi
+done
 
 # Change default shell to zsh
-echo "Change default shell to zsh"
+echo 'Change default shell to zsh'
 chsh -s `which zsh`
 
-echo "Dotfiles installed"
+echo 'Dotfiles installed'
