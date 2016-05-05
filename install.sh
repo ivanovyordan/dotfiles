dccf27ef2679056a245c2126eb86170c6860083c
Yordan Ivanov <mail@ivanovyordan.com>
2016-05-05 08:09:20 +0300

    Some minor bug fixes

--- install.sh
+++ install.sh
@@ -88,6 +88,7 @@ NPM_PACKAGES=(
 	gulp
 	eslint
 	hexo-cli
+	typescript
 )
 
 ## Ruby Gems ##
@@ -149,20 +150,19 @@ NPM_PACKAGES=$(IFS=$' '; echo "${NPM_PACKAGES[*]}")
 npm install -g "$NPM_PACKAGES"
 
 echo "Install Miniconda"
-mkdir $HOME/.applications
 wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
 bash Miniconda-latest-Linux-x86_64.sh -b -p $HOME/.applications/miniconda
 rm Miniconda-latest-Linux-x86_64.sh
 
 echo "Update Miniconda"
-source $HOME/.bashrc
+export PATH="$HOME/.applications/miniconda/bin:$PATH"
 conda update -y conda
 
 echo "Install Miniconda Python 3"
 conda create -y --name python3 python=3
 
 echo "Install RVM"
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source "$HOME/.rvm/scripts/rvm"
 
@@ -172,12 +172,10 @@ gem install "$RUBY_GEMS"
 
 # Setup ADB rules
 sudo wget "http://source.android.com/source/51-android.rules" -O "/etc/udev/rules.d/51-android.rules"
-sudo sed -i "s/<username>/${USERNAME}/g" "/etc/udev/rules.d/51-android.rules"
+sudo sed -i "s/<username>/${USER}/g" "/etc/udev/rules.d/51-android.rules"
 sudo chmod 644 "/etc/udev/rules.d/51-android.rules"
 sudo service udev restart
 
-# Install Vundle
-gitt clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 
 # Clonning the repository #
 echo 'Clonning the repository'
@@ -188,8 +186,10 @@ echo "Create links"
 ln -s "$LINK_SOURCE_DIR/*" "$LINK_DEST_DIR"
 
 # Setup vim
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
+$HOME/.vim/bundle/YouCompleteMe/install.py --all
 vim +PluginInstall +qall
-sh "$HOME/.vim/bundle/YouCompleteMe/install.sh"
 
 echo "Create startup scripts"
 FILES=$(ls -A "$STARTUP_SOURCE_DIR")
