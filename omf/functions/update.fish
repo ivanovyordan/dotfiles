da326693335c1ce51bcc1ae0c2e192f2d4cf629a
Yordan Ivanov <mail@ivanovyordan.com>
2021-08-18 17:09:10 +0300

    Add update function to update all packages

---
+++ omf/functions/update.fish
@@ -0,0 +1,28 @@
+function __update_apt
+  sudo apt update
+  sudo apt upgrade -y
+  sudo apt dist-upgrade -y
+  sudo apt autoremove -y
+  sudo apt autoclean
+end
+
+function __update_flatpak
+  flatpak update -y
+  flatpak uninstall --unused -y
+end
+
+function __update_brew
+  brew update
+  brew upgrade && brew cleanup
+end
+
+function update
+  set kernel_name (uname -s | tr -d '\n')
+
+  __update_brew
+
+  if [ $kernel_name = "Linux" ]
+    __update_apt
+    __update_flatpak
+  end
+end
