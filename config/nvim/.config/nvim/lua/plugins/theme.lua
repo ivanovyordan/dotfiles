cae01d975a2a6abcf4c4ed1df7024a10345c5a55
Yordan Ivanov <mail@ivanovyordan.com>
2024-04-14 20:22:28 +0300

    Change theme

--- config/nvim/.config/nvim/lua/plugins/theme.lua
+++ config/nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,10 @@
 return {
-  'projekt0n/github-nvim-theme',
-
-  lazy = false,
-  priority = 1000,
+  "catppuccin/nvim", name = "catppuccin", priority = 1000,
+  -- "projekt0n/github-nvim-theme", lazy = false, priority = 1000,
 
   config = function()
-    require('github-theme').setup({})
-    vim.cmd 'colorscheme github_dark_high_contrast'
+    -- require('github-theme').setup({})
+    -- vim.cmd "colorscheme github_dark_high_contrast"
+    vim.cmd.colorscheme "catppuccin"
   end
 }
